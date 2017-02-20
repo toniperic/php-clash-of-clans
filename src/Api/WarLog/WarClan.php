15a19ea823a97b63be3f116f0537d4bec89c3ec3
Andrés <andreszacchino@gmail.com>
2017-02-20 00:13:44 -0300

    Create WarClan.php

---
+++ src/Api/WarLog/WarClan.php
@@ -0,0 +1,28 @@
+
+namespace ClashOfClans\Api\WarLog;
+
+use ClashOfClans\Api\AbstractResource;
+
+/**
+ * @method string name()
+ * @method string tag()
+ * @method int clanLevel()
+ * @method int attacks()
+ * @method int stars()
+ * @method int expEarned()
+ */
+class WarClan extends AbstractResource
+{
+    protected $casts = [
+        'badgeUrls' => Badge::class,
+    ];
+    
+    /**
+     * @return Badge|null
+     */
+    public function badge()
+    {
+        return $this->get('badgeUrls');
+    }
+   
+}
